0242a0d96ecb6aff4598843fd9846302025fc650
Marie-Sophie Duplouich <marie-sophieduplouich@Marie-Sophies-MacBook-Pro.local>
2025-07-03 10:46:26 +0200

    variables et somme

--- main.php
+++ main.php
@@ -1,6 +1,12 @@
 <?php
-echo "Salut tout le monde !\n Je m'appelle Marie-Sophie \n Marie-Sophie";
-echo "Bienvenue dans le monde du PHP";
-echo  7+5;
-echo  12-4;
+// echo "Salut tout le monde !\n Je m'appelle Marie-Sophie \n Marie-Sophie";
+// echo "Bienvenue dans le monde du PHP";
+// echo  7+5;
+// echo  12-4;
 
+$x=8;
+$y=6;
+$somme = $x + $y;
+echo ($somme)
+
+?>
