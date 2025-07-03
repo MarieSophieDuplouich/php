fe9f4bedfd6b3cd59f6e7052152cc217a41d81e2
Marie-Sophie Duplouich <marie-sophieduplouich@Marie-Sophies-MacBook-Pro.local>
2025-07-03 11:13:02 +0200

    suite exos php

--- main.php
+++ main.php
@@ -4,9 +4,61 @@
 // echo  7+5;
 // echo  12-4;
 
-$x=8;
-$y=6;
-$somme = $x + $y;
-echo ($somme)
+// $x=8;
+// $y=6;
+// $somme = $x + $y;
+// echo ($somme)
 
-?>
+// $x=25;
+// $y=4;
+// $division = $x / $y;
+// echo ($divisio,)
+
+
+// $x=17;
+// $y=3;
+// $modulo = $x%$y;
+// echo ($modulo)
+
+
+// $naissance= 1990;
+// $anneeactuelle = 2025;
+
+// $age = $anneeactuelle - $naissance ;
+
+// echo "Aujourd'hui, j'ai $age ans.";
+
+
+
+// <!-- Affichez la phrase : La somme de 10, 20 et 30 est : 60 en utilisant trois variables. -->
+
+
+
+
+// <!-- Calculez et affichez la moyenne de trois notes (par exemple 14, 16, 18). -->
+
+// $moyenne = (14 + 16 + 18) /3;
+
+// echo "La moyenne est de $moyenne.\n" ;
+
+
+// <!-- Affichez le résultat de l'expression suivante : (5 + 3) * 2. -->
+
+// $x = (5 + 3) * 2;
+
+// echo $x;
+
+// <!-- Affichez la concaténation de deux chaînes de caractères, par exemple votre prénom et votre nom. -->
+
+// <!-- Affichez la phrase : Le carré de 9 est : 81 en utilisant une variable pour le nombre et une opération pour le calculer. -->
+
+// <!-- Déclarez deux variables $a et $b contenant respectivement 10 et 20, puis affichez leur somme. -->
+
+// <!-- Affichez la phrase : Le résultat de 15 x 3 est : 45 en utilisant des variables pour les nombres. -->
+
+// <!-- Affichez la phrase : Aujourd'hui, il fait beau.. -->
+
+// <!-- Affichez le résultat de la division de 100 par 4. -->
+
+// <!-- Affichez les phrases suivantes sur deux lignes : -->
+
