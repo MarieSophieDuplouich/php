82aaf007ea4b333c596fedb012ada6ed197eceb3
Marie-Sophie Duplouich <marie-sophieduplouich@Marie-Sophies-MacBook-Pro.local>
2025-07-07 16:15:23 +0200

    fonctions commencées

---
+++ fonctions.php
@@ -0,0 +1,70 @@
+<?php
+
+// function myMessage() {
+//   echo "Hello world!";
+// }
+
+// myMessage();
+
+// function Somme(float $a,float $b) : float{
+//     return $a+$b;
+// }
+
+// echo Somme(2,3) + Somme(4,5); // Affiche 14
+// // equivalent à echo 2+3 + 4+5;
+
+
+// $result = addition(5, 10); // Affiche 15
+// echo $result;
+// function addition($a, $b)
+// {
+//     return  $a + $b;
+// }
+
+
+// Créez une fonction estPair qui prend un nombre en paramètre et retourne true si le nombre est pair, false sinon.
+
+
+// Utilisez l'opérateur modulo % pour vérifier si le nombre est pair.
+
+
+
+// Créez une fonction afficherTableMultiplication qui prend un nombre en paramètre et affiche sa table de multiplication de 1 à 10.
+
+
+
+// Créez une fonction calculerMoyenne qui prend un tableau de nombres en paramètre et retourne la moyenne de ces nombres.
+
+
+
+// Vous pouvez utilisez la fonction array_sum() pour calculer la somme des éléments du tableau ou coder la somme manuellement avec une boucle.
+
+
+
+// Créez une fonction convertirCelsiusEnFahrenheit qui prend une température en degrés Celsius et retourne sa valeur en Fahrenheit.
+// Vous allez devoir vous intéresser à la formule de conversion de Fahrenheit en Celsius. https://www.rapidtables.org/fr/convert/temperature/celsius-to-fahrenheit.html
+
+
+
+// Créez une fonction compterVoyelles qui prend une chaîne de caractères et retourne le nombre de voyelles qu'elle contient.
+// Astuce : Utilisez la fonction strtolower() pour convertir la chaîne en minuscules avant de compter les voyelles.
+
+
+
+// Créez une fonction inverserTableau qui prend un tableau en paramètre et retourne un nouveau tableau avec les éléments dans l'ordre inverse.
+// C'est la même fonction que array_reverse() mais vous devez la coder vous-même. w3schools : https://www.w3schools.com/php/func_math_max.asp
+
+
+
+// Créez une fonction estPalindrome qui prend une chaîne de caractères et retourne true si c'est un palindrome, false sinon.
+// Un palindrom est mot qui se lit de la même manière même une fois retourné, par exemple "radar", "level", "kayak". Utilisez la fonction strrev() pour inverser une chaîne de caractères.
+
+
+
+
+// Créez une fonction trouverMaximum qui prend un tableau de nombres et retourne la plus grande valeur du tableau.
+// C'est la même fonction que max() mais vous devez la coder vous-même. w3schools : https://www.w3schools.com/php/func_array_max.asp
+
+
+
+// Créez une fonction fusionnerTableaux qui prend deux tableaux en paramètre et retourne un tableau contenant les éléments des deux tableaux.
